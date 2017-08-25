7952fe236333366b85081684637d2066d9852d05
Aden King <adenking@live.com>
2017-08-25 17:47:59 +1000

    fixed up converting string to list

--- Line_Deleter.py
+++ Line_Deleter.py
@@ -1,17 +1,18 @@
 def process_lines(lines, line_length):
-    proccessed_lines = []
+    processed_lines = []
     j = 0
-    for i in range(0, len(lines)):
+    for i in len(lines):
         if len(i) > line_length:
-            proccessed_lines[j] += lines[i]
+            processed_lines[j] = lines[i]
             j += 1
-    return proccessed_lines
+    return processed_lines
 
 
 def line_delete(filename, line_length):
     with open(filename)as f:
-        print(process_lines(f.read(), line_length))
-
+        line_data = f.read()
+    line_data = line_data.splitlines()
+    print(process_lines(line_data, line_length))
 
 
 line_delete("wordlist_test.txt", 3)
